a6c267b277c3aab7e732e6745e0ff378df7561a1
Nimus-oes <sseo.code@gmail.com>
2025-08-28 14:20:51 +0900

    Update color constant

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,9 @@
 export const FILTERS = ["all", "fav"] as const;
 export const SORTING_ORDERS = ["desc", "asc"] as const;
 export const NOTE_COLORS = [
-  "#F7CB7F", // yellow
-  "#EFA07A", // orange
-  "#B096F6", // purple
-  "#5FD1FA", // blue
-  "#E6EE99", // green
+  "#F7CB7F",
+  "#EFA07A",
+  "#ef7a7aff",
+  "#B096F6",
+  "#E6EE99",
 ];
