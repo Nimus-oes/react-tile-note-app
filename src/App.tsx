37846882888b342fba2aa162cac4667120cf728f
Nimus-oes <sseo.code@gmail.com>
2025-08-29 17:00:35 +0900

    Store notes in localStorage and initialize state with it

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import NoteCardList from "./components/NoteCardList/NoteCardList";
 import type { Note, Filter, Sorter } from "./types";
 import InputForm from "./components/InputForm/InputForm";
@@ -7,31 +7,31 @@ import Header from "./components/Header/Header";
 import DarkThemeProvider from "./context/DarkThemeProvider";
 
 export default function App() {
-  const [notes, setNotes] = useState<Note[]>([]);
+  const [notes, setNotes] = useState<Note[]>(getNotesFromLocalStorage);
   const [filter, setFilter] = useState<Filter>("all");
   const [sorter, setSorter] = useState<Sorter>("desc");
 
   const addNote = (noteToAdd: Note) => {
     setNotes((prev) => [...prev, noteToAdd]);
   };
-
   const deleteNote = (noteId: string) => {
     setNotes(notes.filter((item) => item.id !== noteId));
   };
-
   const setFavorite = (noteToUpdate: Note) => {
     setNotes(
       notes.map((item) => (item.id === noteToUpdate.id ? noteToUpdate : item)),
     );
   };
-
   const updateFilter = (filter: Filter) => {
     setFilter(filter);
   };
-
   const updateSorter = (sorter: Sorter) => {
     setSorter(sorter);
   };
+
+  useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
 
   return (
     <DarkThemeProvider>
@@ -53,4 +53,9 @@ export default function App() {
       </main>
     </DarkThemeProvider>
   );
+}
+
+function getNotesFromLocalStorage() {
+  const notes = localStorage.getItem("notes");
+  return notes ? JSON.parse(notes) : [];
 }
